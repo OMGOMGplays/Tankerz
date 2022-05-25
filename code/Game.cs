b01c7cbb6db89569c0b9ed36c05451a51a64d831
OMGOMGplays <loke@jenjen.se>
2022-05-26 00:00:26 +0200

    Initial commit
    - The First Commit Ever!!!

---
+++ code/Game.cs
@@ -0,0 +1,24 @@
+﻿using Sandbox;
+using System;
+using System.Linq;
+
+namespace TZ 
+{
+	public partial class TZGame : Game 
+	{
+		public TZGame() 
+		{
+
+		}
+
+		public override void ClientJoined( Client cl )
+		{
+			base.ClientJoined( cl );
+
+			var player = new TZPlayer();
+			player.Respawn();
+
+			cl.Pawn = player;
+		}
+	}
+}
---
+++ code/Player/Player.cs
@@ -0,0 +1,18 @@
+﻿using Sandbox;
+
+namespace TZ 
+{
+	public partial class TZPlayer : Player 
+	{
+		public override void Respawn()
+		{
+			base.Respawn();
+
+			SetModel("models/citizen/citizen.vmdl"); // TEMP \\ Switch to tank model when imported - Lokiv
+
+			CameraMode = new ThirdPersonCamera();
+			Animator = new StandardPlayerAnimator();
+			Controller = new WalkController();
+		}
+	}
+}
